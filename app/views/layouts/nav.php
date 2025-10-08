ad63b628baa3c95d871e4300b44b138d7c626d6a
Junior-Sg <santamariajunior561@gmail.com>
2025-10-08 13:07:09 -0500

    actualizacion BD y carpetas

---
+++ app/views/layouts/nav.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bibliotec_KJ</title>
+    <link rel="stylesheet" href="../../../public/css/style_Layaout/barranav.css">
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@700&family=Poppins:wght@400;500&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="header">
+  <img src="../../../public/img/Logos/L1.jpg" alt="Logo_biblioteckj" class="logo">
+  <h1 class="titulo">Bibliotec_KJ</h1>
+</div>
+
+<nav class="navbar">
+  <ul class="menu">
+    <li><a href="/">Inicio</a></li>
+    <li><a href="/">Librería</a></li>
+    <li class="despliegue">
+      <a href="#">Género</a>
+      <div class="despliegue-menu">
+        <a href="#">Comedia</a>
+        <a href="#">Drama</a>
+        <a href="#">Acción</a>
+        <a href="#">Romance</a>
+        <a href="#">Biología</a>
+        <a href="#">Musical</a>
+        <a href="#">Biografía</a>
+        <a href="#">Historia</a>
+        <a href="#">Matemáticas</a>
+        <a href="#">Ciencias Sociales</a>
+        <a href="#">Ciencias Naturales</a>
+        <a href="#">Informática</a>
+        <a href="#">Artística</a>
+      </div>
+    </li>
+    <li class="despliegue">
+      <a href="#">Iniciar Sesión</a>
+      <div class="despliegue-menu">
+        <a href="">Usuario</a>
+        <a href="">Administrador</a>
+      </div>
+    </li>
+  </ul>
+</nav>
+</body>
+</html>
